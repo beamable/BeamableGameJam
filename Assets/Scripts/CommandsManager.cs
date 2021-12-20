71a9c711c5c34fe55c0759cd9fe00ed8858eb55e
Łukasz Kundys <lukaszkundys@gmail.com>
2021-12-20 19:03:14 +0100

    MorseDecoder, CommandsManager and UI assets

---
+++ Assets/Scripts/CommandsManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandsManager : MonoBehaviour
+{
+#pragma warning disable CS0649
+    [SerializeField] private MorseDecoder morseDecoder;
+    [SerializeField] private List<string> _commands = new List<string>();
+#pragma warning restore CS0649
+    
+    private readonly Dictionary<string, Action> _registeredActions = new Dictionary<string, Action>();
+
+    public void IssueCommand()
+    {
+        string currentCommand = morseDecoder.GetCurrentCommand().ToUpper();
+        if (_commands.Contains(currentCommand))
+        {
+            if(_registeredActions.TryGetValue(currentCommand, out Action currentAction))
+            {
+                currentAction?.Invoke();
+            }
+            else
+            {
+                Debug.Log($"Command {currentCommand} has no action registered");
+            }
+        }
+        else
+        {
+            Debug.Log($"Command {currentCommand} doesn't exist");
+        }
+    }
+
+    public void RegisterActionForCommand(string command, Action action)
+    {
+        if (!_commands.Contains(command))
+        {
+            Debug.Log($"Command {command} is not allowed");
+            return;
+        }
+        
+        _registeredActions.Add(command.ToUpper(), action);
+    }
+}
