e602e90504a2891069a1171f38f11e01bb969565
Łukasz Kundys <lukaszkundys@gmail.com>
2021-12-21 16:13:21 +0100

    HInts with possible orders

---
+++ Assets/Scripts/Gameplay/HintsPresenter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HintsPresenter : MonoBehaviour
+{
+    [SerializeField] private MorseDecoder _morseDecoder;
+    [SerializeField] private Hint _hintPrefab;
+    [SerializeField] private RectTransform _hintsParent;
+    
+    private void Start()
+    {
+        ConfigsRepository configsRepository = FindObjectOfType<ConfigsRepository>();
+        List<string> availableCommands = configsRepository?.CommandsConfig?.AvailableCommands;
+
+        if (availableCommands == null) return;
+        
+        foreach (string command in availableCommands)
+        {
+            Hint hint = Instantiate(_hintPrefab, _hintsParent, false);
+
+            string code = _morseDecoder.GetCodeForCommand(command);
+            hint.Setup($"{command}: {code}");
+        }
+    }
+}
