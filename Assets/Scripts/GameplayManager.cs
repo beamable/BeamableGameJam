b0912be74207abe68007ee1fd29aedc0c83a4083
Łukasz Kundys <lukaszkundys@gmail.com>
2021-12-22 09:43:11 +0100

    Scenes connected, orders issued with morse code

---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    [SerializeField] private CommandsManager _commandsManager;
+    [SerializeField] private TankController _tankController;
+    
+    private void Start()
+    {
+        _commandsManager.RegisterActionForCommand("A", _tankController.Attack);
+        _commandsManager.RegisterActionForCommand("L", _tankController.Reload);
+        _commandsManager.RegisterActionForCommand("P", _tankController.Boost);
+    }
+}
